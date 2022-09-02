213723f689c98c086d294c82a0f5fdd2a9a19177
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2022-09-02 17:53:38 +0700

    add more bad domains + script improvement
    + Improve scripts/checkDomains.sh
    + After rechecking the "unexistent" domains, i found some that actually still up (well its because my pc hosts config), and added those back (also uhh... ddosing 8.8.8.8 for bruteforce-finding 1-100-courier.push.apple.com coz theres no other way than doing this).
    + And usual adding bad domains...

--- scripts/checkDomains.sh
+++ scripts/checkDomains.sh
@@ -3,25 +3,41 @@
 #  What this script does?
 #  it tries to scan every DNS response and
 #  find if a domain exists or not
-#  and soon going to be used against BaDomain lists
+#  and used to check BaDomain lists
 #
+
+echo -e "[i] Initializing..."
 
 ## Configuration ##
 DNS_PROVIDER="@8.8.8.8" # nah its not private, but its really fast especially to spam all that domains bruhhh
 INPUT_FILE="$1"
 LOG_NONE_FILE="resultNone.log"
+LOG_SERVFAIL_FILE="resultServFail.log"
 LOG_EXISTS_FILE="resultExists.log"
-DOMAIN_LISTS="google.com youtube.com ThisDomainDoesntExistAtAll.dontreturnsomethingok"
+DOMAIN_LISTS="google.com youtube.com ThisDomainDoesntExistAtAll.dontreturnsomethingok" # just for an example
 
-## Query stuff... ##
-echo -e "[i] Initializing..."
-echo -e "[i] Querying $INPUT_FILE lists..."
+## Query domain lists, remove previous log, and stuff... ##
+echo "[i] Querying $INPUT_FILE domain lists..."
 DOMAIN_LISTS="$(sed '/^#/d' $INPUT_FILE | sed -r '/^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/d')"
+[ -f "$LOG_NONE_FILE" ] && echo "[i] Removing $LOG_NONE_FILE..." && rm "$LOG_NONE_FILE";
+[ -f "$LOG_SERVFAIL_FILE" ] && echo "[i] Removing $LOG_SERVFAIL_FILE..." && rm "$LOG_SERVFAIL_FILE";
+[ -f "$LOG_EXISTS_FILE" ] && echo "[i] Removing $LOG_EXISTS_FILE..." && rm "$LOG_EXISTS_FILE";
+echo "[i] Using $DNS_PROVIDER as DNS Resolver..."
 
 ## and check one per one ##
 for i in $DOMAIN_LISTS;do
 	echo -en "[i] Checking $i...\e[0K\r"
-	result="$(dig $DNS_PROVIDER $i | grep 'ANSWER: 0' &>/dev/null;echo $?)"
-	[ "$result" != "0" ] && echo -e "[+] $i Exists\e[0K\r" || echo -e "[-] $i Not Exist\e[0K\r"
-	[ "$result" != "0" ] && echo $i >> "$LOG_EXISTS_FILE" || echo $i >> "$LOG_NONE_FILE"
+	result="$(dig $DNS_PROVIDER $i)"
+	result_isNone="$(echo $result | grep 'ANSWER: 0' &>/dev/null;echo $?)"
+	result_status="$(echo $result | grep ', status: ' &>/dev/null;echo $?)"
+	if [ "$result_isNone" != "0" ];then
+		echo -e "[+] $i exists\e[0K\r"
+		echo $i >> "$LOG_EXISTS_FILE"
+	elif echo $result_status | grep ', status: SERVFAIL' &>/dev/null;then
+		echo -e "[!] DNS Resolver is out-of-time to query $i\e[0K\r"
+		echo $i >> "$LOG_SERVFAIL_FILE"
+	else
+		echo -e "[-] $i not exist\e[0K\r"
+		echo $i >> "$LOG_NONE_FILE"
+	fi
 done
