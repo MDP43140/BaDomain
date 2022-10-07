a4d093c17e8245c10e3d619365380dd0690f9dcc
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2022-10-07 16:18:00 +0700

    Add more bad domains
    well like usual here we go again more bad domains to be added, even though its not much, mostly indian scamshit and 1 ridicilous rando ads server again
    ...plus minor adaway json parser script fix where it wont show any blocked domains with allowed list being nothing (allowed: []).

--- scripts/parseAdAwayJson.sh
+++ scripts/parseAdAwayJson.sh
@@ -3,6 +3,16 @@
 #  What this script does?
 #  Extracts blocked domain lists from AdAway exported JSON file
 #
+#  What each command below does?
+#  + Print the adaway json input file
+#  + Remove trailing tabs/spaces
+#  + remove `"enabled": true` << this is just an example
+#  + remove `"url": "http://example.com"`
+#  + remove `>"host": <"example.com"`
+#  + remove quotes (not sure)
+#  + remove ending comma
+#  + get the list in allowed/disallowed range (those will be blocked domains)
+#  + and sort it
 
 clear;
 cat $1 \
@@ -10,10 +20,10 @@ cat $1 \
  | sed -r '/\"(enabled|allow|redirect)\"\: (true|false)/d' \
  | sed -r '/\"(url|label|redirect)\"\: \".+\"/d' \
  | sed -r 's/\"host\"\: \"//g' \
- | sed -r '/(\{|(\]|\}))/d' \
  | sed -r 's/['\''\"]//g' \
  | sed -r 's/,$//g' \
  | grep 'allowed: \[' --before-context=999999 \
  | grep 'blocked: \[' --after-context=999999 \
  | sed -r 's/^(allow|block)ed\:\ \[//g' \
+ | sed -r '/\{|\]|\}/d' \
  | sort -ui;
