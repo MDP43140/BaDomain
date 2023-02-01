b7ee8509ab49fe810b9801daaec90b7a0b679196
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2023-02-01 20:30:04 +0700

    Update BaDomain
    + [genFbCdnList.sh] just minor code cleanup...
    + [checkDomains.sh] Replace $(cmd) > `cmd`.
    + [checkDomains.sh] Add simple goverment censorship redirect detection (currently its only able to detect Indonesia government redirect domains, from Kominfo, XL).
    + [checkDomains.sh] fix some detection potentally not working (by adding quotes on a variable: echo "$var").
    + [checkDomains.sh] change the way grep output is silenced (`grep PATTERN &/dev/null` > `grep -q PATTERN`).
    + And also the good old: adding bad domains to the blacklist... mostly chinese sites, gambling (and it's CDNs too: pookcdn), malware (Domain suspected to be C2C, microsofthk, microsoftkernel), sus-looking websites, advertising (inmobi), telemetry (Eyeo UCBrowser), couple more apple, FACEBOOK (yet again...), xiaomi domains. and finally 2 not existent domains (chinese & gambling site).

--- scripts/checkDomains.sh
+++ scripts/checkDomains.sh
@@ -18,13 +18,13 @@ LOG_EXISTS_FILE="resultExists.log"
 DOMAIN_LISTS="google.com youtube.com example.com ThisDomainDoesntExistAtAll.dontreturnsomethingok" # just for an example
 
 ## Query domain lists, remove previous log, and stuff... ##
-echo "[i] Querying $([ "$INPUT_FILE" ] && echo $INPUT_FILE || echo StandardInput) domain lists..."
-DOMAIN_LISTS="$(sed '/^#/d' $INPUT_FILE | sed -r '/^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/d' | sort -ui)"
+echo "[i] Querying `[ "$INPUT_FILE" ] && echo $INPUT_FILE || echo StandardInput` domain lists..."
+DOMAIN_LISTS=`sed '/^#/d' $INPUT_FILE | sed -r '/^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/d' | sort -ui`
 
 # Count total domains
 # Sometimes, DOMAIN_LISTS might be separated by spaces and will return 1 as a count if running wc-l. in that case, rerun wc-w (this will count per space instead of per newline)
-TOTAL_DOMAINS="$(echo $DOMAIN_LISTS | wc -l)"
-[ "$TOTAL_DOMAINS" -le "1" ] && TOTAL_DOMAINS="$(echo $DOMAIN_LISTS | wc -w)"
+TOTAL_DOMAINS=`echo $DOMAIN_LISTS | wc -l`
+[ "$TOTAL_DOMAINS" -le 1 ] && TOTAL_DOMAINS=`echo $DOMAIN_LISTS | wc -w`
 
 # this script is able to work with stdin (./checkDomains.sh << EOF google.com youtube.com EOF), but there might be a bit of problems afaik
 [ "$INPUT_FILE" = '' -a "$DOMAIN_LISTS" != '' ] && echo "[!] Looks like you're using stdin, please note that this might be little bit unstable"
@@ -39,16 +39,17 @@ PROGRESS=0
 for i in $DOMAIN_LISTS;do
 	PROGRESS=$((PROGRESS+1))
 	echo -en "\r[i] ($PROGRESS/$TOTAL_DOMAINS) Checking \e[1m$i\e[0;2;5m...\e[0m\e[0K"
-	result="$(dig $DNS_PROVIDER $i)"
-	result_isNone="$(echo $result | grep 'ANSWER: 0' &>/dev/null;echo $?)"
-	result_status="$(echo $result | grep ', status: ' &>/dev/null;echo $?)"
+	result=`dig $DNS_PROVIDER $i`
 	if [ ! "$result" ];then
 		echo -e "\r[!] No answer for $i. Check your internet\e[0K"
 		echo $i >> "$LOG_SERVFAIL_FILE"
-	elif [ "$result_isNone" != "0" ];then
+	elif echo "$result" | grep -q 'trustpositifkominfo\|blockpage\|aduankonten\|blockpage\.xl\.co\.id\|aduankonten\.mail\.kominfo\.go\.id';then
+		echo -e "\r[!] $i censored by government\e[0K"
+		echo $i >> "$LOG_EXISTS_FILE"
+	elif ! echo "$result" | grep -q 'ANSWER: 0';then
 		echo -e "\r[+] $i exists\e[0K"
 		echo $i >> "$LOG_EXISTS_FILE"
-	elif echo $result_status | grep ', status: SERVFAIL' &>/dev/null;then
+	elif echo "$result" | grep -q ', status: SERVFAIL';then
 		echo -e "\r[!] DNS Resolver is out-of-time to query $i\e[0K"
 		echo $i >> "$LOG_SERVFAIL_FILE"
 	else
--- scripts/genFbCdnList.sh
+++ scripts/genFbCdnList.sh
@@ -38,12 +38,11 @@
 SFB_TYPES=(
 	# PS: these 3 queries below returns same response if the server exists
 	# eg: `external.fmof1-1.fna.fbcdn.net` & `FUCKTHEZUCK.fmof1-1.fna.fbcdn.net` exist if `fmof1-1.fna.fbcdn.net` exist (it doesn't exist if you JUST query `fmof1-1.fna.fbcdn.net` --
-	# it needs to have a subdomain (can be anything): `WHATEVERSUBDOMAINHEREBTWZUCKISMORONIST.fmof1-1.fna.fbcdn.net`)
+	# it needs to have a subdomain (can be anything): `WHATEVRSUBDOMAINHEREBTWZUCKISADEVIL.fmof1-1.fna.fbcdn.net`)
 	# When done querying, the existing result then should be duplicated 3 times and replace each duplicated "external" to "instagram","scontent",etc respectively.
 #	"external.f" # MainDomain1
 #	"instagram.f" # MainDomain1 (not recommended, replaced by external.f)
 #	"scontent.f" # MainDomain1 (not recommended, replaced by external.f)
-
 #	"media.f" # MainDomain2
 #	"media-" # MainDomain2
 	"sonar6-" # MainDomain3
@@ -191,27 +190,29 @@ SFB_MAINDOMAIN=(
 	"xx.fbcdn.net"
 	"cdninstagram.com"
 )
-# this will generate "1 2 ... 32 33"
-SFB_BRUTEFORCERANGE_1="$(echo {1..33})"
-SFB_BRUTEFORCERANGE_2="$(echo {1..6})"
-
 
 ## Main script ##
+gen(){
+	# this will generate "1 2 ... 32 33"
+	SFB_BRUTEFORCERANGE_1=`echo {1..33}`
+	SFB_BRUTEFORCERANGE_2=`echo {1..6}`
 
-# Join array to string divided by spaces
-SFB_TYPES="${SFB_TYPES[@]}"
-SFB_REGIONCODE="${SFB_REGIONCODE[@]}"
-SFB_MAINDOMAIN="${SFB_MAINDOMAIN[@]}"
+	# Join array to string divided by spaces
+	SFB_TYPES="${SFB_TYPES[@]}"
+	SFB_REGIONCODE="${SFB_REGIONCODE[@]}"
+	SFB_MAINDOMAIN="${SFB_MAINDOMAIN[@]}"
 
-for type in $SFB_TYPES;do
-	for region in $SFB_REGIONCODE;do
-		for domain in $SFB_MAINDOMAIN;do
-			for n1 in $SFB_BRUTEFORCERANGE_1;do
-				for n2 in $SFB_BRUTEFORCERANGE_2;do
-					echo "${type}${region}${n1}-${n2}.${domain}" # output example: scontent.fcgk4-4.fna.fbcdn.net
+	for type in $SFB_TYPES;do
+		for region in $SFB_REGIONCODE;do
+			for domain in $SFB_MAINDOMAIN;do
+				for n1 in $SFB_BRUTEFORCERANGE_1;do
+					for n2 in $SFB_BRUTEFORCERANGE_2;do
+						echo "${type}${region}${n1}-${n2}.${domain}" # output example: scontent.fcgk4-4.fna.fbcdn.net
+					done
 				done
+			#	echo $type$region.$domain # output example: scontent-cgk.xx.fbcdn.net
 			done
-		#	echo $type$region.$domain # output example: scontent-cgk.xx.fbcdn.net
 		done
 	done
-done
+}
+gen
