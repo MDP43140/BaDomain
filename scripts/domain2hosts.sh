e4cd21596e3d9b6d16f24c9e140ef15529ba152e
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2024-09-24 21:10:18 +0700

    Update BaDomain
    + Just a quick addition, because just recently
    	i got yet another one of those illegal gambling
    	spam sms messages again (ugh... :sad:), this really
    	made me so annoyed, so i decided to immediately
    	block the domain so that no one falls for the scam.
    + Added 1 facebook domain, 3 gambling domains (one of them is
    	CloudFront domain, see the misc/NoGambling.txt
    	commit for explanation), and 14 nsfw domains
    + Added 6 bad domains (ads/tracking)
    + [domain2hosts] Adjusted to not output comment
    	after domain name (in cases of parsing bugs
    	on certain programs)(this also updates misc/NoFB.txt)

--- scripts/domain2hosts.sh
+++ scripts/domain2hosts.sh
@@ -8,10 +8,10 @@
 if [ -f $1.txt -a ! -f $1_hosts.txt ];then
 	echo -en "\r[i] Creating $1 hosts file...\e[0K"
 	# put comments to result file, add newline, remove comments,
-	# add host target, put it to new hosts file
+	# add host target (how to remove comment after it?), put it to new hosts file
 	grep "^ *#" $1.txt > $1_hosts.txt
 	echo >> $1_hosts.txt
-	grep -v '^$' $1.txt | sed '/^#/d;s/^/0.0.0.0 /' >> $1_hosts.txt
+	grep -v '^$' $1.txt | sed -r '/^#/d;s/^/0.0.0.0 /;s/ ?#.+$//' >> $1_hosts.txt
 	echo -e "\r[+] $1 hosts file created!\e[0K"
 else
 	echo -e "[!] $1_hosts.txt exist or $1.txt did'nt exist, won't continue for failsafe.\e[0K"
