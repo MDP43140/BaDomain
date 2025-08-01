a20eaabce730f576839eaeac29d762e9bde7b728
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2025-08-01 21:33:50 +0700

    Update BaDomain
    + Added to blocklists:
    	+ Advanced: 2
    	+ Apple: 5
    	+ Facebook: 22
    	+ Free fire: 2
    	+ Malware: 12
    	+ Nsfw: 1
    	+ Samsung: 1
    	+ Tiktok: 37
    	+ Vivo: 1
    	+ Xiaomi: 1
    	+ Nonexistent: 2
    	+ Main blocklist: 33 (ads, tracking, ip logger, transsion, glance)
    	I might start doing similiar format again
    	because automated git script does help a bit
    	and writing the commit log manually is a pain,
    	and i simply dont have the time for it.
    + Fixed 1 misplaced tiktok domain on facebook domain
    + Script fixes

--- scripts/adAwayDb_extractUserBlockedHosts.sh
+++ scripts/adAwayDb_extractUserBlockedHosts.sh
@@ -3,6 +3,32 @@
 # SPDX-FileCopyrightText: 2021-2025 MDP43140
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
+# host_entries:
+# host|TEXT|1||1
+# type|INTEGER|1||0
+# redirection|TEXT|0||0
+#
+# hosts_lists:
+# id|INTEGER|1||1
+# host|TEXT|1||0
+# type|INTEGER|1||0
+# enabled|INTEGER|1||0
+# redirection|TEXT|0||0
+# source_id|INTEGER|1||0
+#
+# hosts_sources:
+# id|INTEGER|1||1
+# label|TEXT|1||0
+# url|TEXT|1||0
+# enabled|INTEGER|1||0
+# allowEnabled|INTEGER|1||0
+# redirectEnabled|INTEGER|1||0
+# last_modified_local|INTEGER|0||0
+# last_modified_online|INTEGER|0||0
+# size|INTEGER|1||0
+# entityTag|TEXT|0|NULL|0
+#
+
 su -c "PATH=\"$PATH\" sqlite3 \
 	/data/data/org.adaway/databases/app.db \
 	'SELECT host FROM hosts_lists WHERE type = 0 AND enabled = 1 AND source_id = 1 AND redirection IS NULL;';\
