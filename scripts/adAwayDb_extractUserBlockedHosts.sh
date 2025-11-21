55d23c8ac9b33d71261c0a782979060eed3e2cfd
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2025-11-21 20:37:01 +0700

    Update BaDomain
    + Added to blocklists:
    	+ Apple: 16
    	+ ChinaOverseaSpy: 1
    	+ FB: 96
    	+ Malware: 14
    	+ OppoHT: 3
    	+ Scam: 3
    	+ TikTok: 8
    	+ Vivo: 1
    	+ Xiaomi: 1
    	+ Not exists: 2
    	+ Main blocklist: 14 (ads/tracker)
    + [adAwayDb_extractUserBlockedHosts.sh] Sort result

--- scripts/adAwayDb_extractUserBlockedHosts.sh
+++ scripts/adAwayDb_extractUserBlockedHosts.sh
@@ -31,5 +31,5 @@
 
 su -c "PATH=\"$PATH\" sqlite3 \
 	/data/data/org.adaway/databases/app.db \
-	'SELECT host FROM hosts_lists WHERE type = 0 AND enabled = 1 AND source_id = 1 AND redirection IS NULL;';\
+	'SELECT host FROM hosts_lists WHERE type = 0 AND enabled = 1 AND source_id = 1 AND redirection IS NULL ORDER BY host COLLATE NOCASE ASC;';\
 ";
